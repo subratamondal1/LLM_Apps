e7f19e82ec556bae7c1476369b25058d29c8228a
Subrata Mondal <subratasubha2@gmail.com>
2023-12-14 23:00:07 +0530

    add chromadb vectorstore

--- 3 facts/app.py
+++ 3 facts/app.py
@@ -2,30 +2,50 @@ from langchain.document_loaders import TextLoader, PyPDFLoader, JSONLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.embeddings.google_palm import GooglePalmEmbeddings
+from langchain.vectorstores.chroma import Chroma
 
 embeddings = GooglePalmEmbeddings()
-embedded_text = embeddings.embed_query(
-    text = "Hi, I am Subrata Mondal"
+
+# embedded_text = embeddings.embed_query(
+#     text = "Hi, I am Subrata Mondal"
+# )
+# print(len(embedded_text))
+
+# Initialize the TextLoader
+loader = TextLoader(
+    file_path = "3 facts/facts.txt"
 )
-print(len(embedded_text), embedded_text)
 
-# # Initialize CharacterTextSplitter
-# character_text_splitter = CharacterTextSplitter(
-#     separator = "\n",
-#     chunk_size = 200, # First it will collect 200 texts and then the separator will be applied
-#     chunk_overlap = 0
-# )
+# Initialize CharacterTextSplitter
+character_text_splitter = CharacterTextSplitter(
+    separator = "\n",
+    chunk_size = 200, # First it will collect 200 texts and then the separator will be applied
+    chunk_overlap = 0
+)
 
-# # Initialize the TextLoader
-# loader = TextLoader(
-#     file_path = "3 facts/facts.txt"
-# )
+# Load the documents
+docs = loader.load_and_split(
+    text_splitter = character_text_splitter
+)
 
-# # Load the documents
-# docs = loader.load_and_split(
-#     text_splitter = character_text_splitter
-# )
+db = Chroma.from_documents(
+    documents=docs, # list of tokens
+    embedding=embeddings,
+    persist_directory="emb"
+)
 
-# for doc in docs:
-#     print(doc)
-#     print()
+results = db.similarity_search_with_score(
+    query="How many people does lightning kills every year?"
+)
+
+print(len(results))
+
+for result in results:
+    print()
+    print(result)
+    print()
+    print(result[0].page_content)
+    print()
+    print(result[1])
+    break
+
